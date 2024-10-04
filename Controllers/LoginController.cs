02d262366822655b02852a4a4ab63ca97a5fe1e7
Vipin Kumar <rudrapal490@gmail.com>
2024-10-04 22:53:43 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace GoogleAuth.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task Login()
+        {
+            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme,
+                new AuthenticationProperties
+                {
+                    RedirectUri = Url.Action("GoogleResponse")
+                });
+        }
+
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claims = result.Principal.Identities.FirstOrDefault().Claims.Select(Claim => new
+            {
+                Claim.Issuer,
+                Claim.OriginalIssuer,
+                Claim.Type,
+                Claim.Value
+            });
+           // return Json(claims);
+
+           return RedirectToAction("Index", "Home");
+        }
+    }
+}
